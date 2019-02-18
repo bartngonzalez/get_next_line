947ae05751df14adec0be2666f97760c35eb6f9b
ng6art <2278bartgonzalez@gmail.com>
2019-02-17 19:02:23 -0800

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bgonzale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 21:45:23 by bgonzale          #+#    #+#             */
-/*   Updated: 2018/11/02 23:52:19 by bgonzale         ###   ########.fr       */
+/*   Updated: 2019/02/17 19:02:07 by bgonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 42
-# include "libft.h"
+# include "libft/includes/libft.h"
 
 int		ft_nl(char **stack, char **line, int fd, int num_bytes);
 int		get_next_line(const int fd, char **line);
